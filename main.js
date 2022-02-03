73e3937ec293a85b739dcc059ec77f4d8249bef6
hafiskarim <hafis.karim5@gmail.com>
2022-02-03 18:38:22 +0700

    add coming soon

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
-  $('.owl-carousel').owlCarousel({
+  $('.owl-carousel.autoplay').owlCarousel({
+    autoplay: true,
     loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+    }
+  })
+  $('.owl-carousel').owlCarousel({
     margin: 10,
     nav: true,
     dots: true,
