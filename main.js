07bb53256cf408574e21cff073853236fa8858ac
hafiskarim <hafis.karim5@gmail.com>
2022-02-03 15:56:17 +0700

    add owl carousel

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+    }
+  })
+})
